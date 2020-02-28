f2c024edab2c6939999114208284f3b764baa8f2
Foxclip <destroyApp2@gmail.com>
2020-02-28 16:13:04 +1000

    Spread

--- trading.py
+++ trading.py
@@ -2,10 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import enum
 from statistics import mean
+import math
 
 
 def to_curr(amount, precision):
-    return amount * 10**precision
+    return int(round(amount * 10**precision))
 
 
 def from_curr(object, precision):
@@ -40,18 +41,28 @@ class Order:
 
 class Simulation:
 
-    def __init__(self, price_data, starting_balance, ma_length, precision):
+    def __init__(self, price_data, ask_price_data, bid_price_data,
+                 starting_balance, ma_length, precision, ignore_spread=False):
         self.index = 0
         self.orders = []
         self.ma_record = []
         self.balance_record = []
         self.price_data = price_data
+        self.ask_price_data = ask_price_data
+        self.bid_price_data = bid_price_data
         self.balance = starting_balance
         self.ma_length = ma_length
         self.precision = precision
+        self.ignore_spread = ignore_spread
 
     def price(self, lookback=0):
         return self.price_data[self.index - lookback]
+
+    def ask_price(self, lookback=0):
+        return self.ask_price_data[self.index - lookback]
+
+    def bid_price(self, lookback=0):
+        return self.bid_price_data[self.index - lookback]
 
     def floating_PL(self):
         return sum([order.calculate_floating_PL(self.price())
@@ -68,13 +79,15 @@ class Simulation:
             return mean(self.price_data[self.index - length:self.index + 1])
 
     def buy(self, amount):
-        new_order = Order(amount, self.price(), OrderType.BUY)
+        price = self.price() if self.ignore_spread else self.ask_price()
+        new_order = Order(amount, price, OrderType.BUY)
         self.orders.append(new_order)
         print("BUY")
         # plt.scatter(self.index, self.price(), color="green")
 
     def sell(self, amount):
-        new_order = Order(amount, self.price(), OrderType.SELL)
+        price = self.price() if self.ignore_spread else self.bid_price()
+        new_order = Order(amount, price, OrderType.SELL)
         self.orders.append(new_order)
         print("SELL")
         # plt.scatter(self.index, self.price(), color="blue")
@@ -93,6 +106,10 @@ class Simulation:
             return True
         else:
             return False
+
+    def run(self):
+        while(self.advance()):
+            pass
 
     def record(self):
         self.ma_record.append(self.moving_average(self.ma_length))
@@ -135,12 +152,25 @@ class Simulation:
 if __name__ == "__main__":
     df = pd.read_csv("EURUSD_i_M1_201706131104_202002240839.csv", sep="\t")
     precision = 5
-    eur_usd = [int(round(x * 10**precision, 0))
-               for x in list(df["<CLOSE>"])[-10000:]]
-    # plt.plot(eur_usd)
-    simulation1 = Simulation(eur_usd, to_curr(1000, precision), 10, precision)
-    while(simulation1.advance()):
-        pass
+    amount = 100000
+    eur_usd = [to_curr(x, precision) for x in list(df["<CLOSE>"])][-amount:]
+    eur_usd_ask = []
+    eur_usd_bid = []
+    for bar_i in range(len(eur_usd)):
+        price = eur_usd[bar_i]
+        spread = df["<SPREAD>"][bar_i]
+        spread_add = math.ceil(spread / 2)
+        eur_usd_ask.append(eur_usd[bar_i] + spread_add)
+        eur_usd_bid.append(eur_usd[bar_i] - spread_add)
+    simulation1 = Simulation(eur_usd, eur_usd_ask, eur_usd_bid,
+                             to_curr(1000, precision), 10, precision,
+                             ignore_spread=True)
+    simulation2 = Simulation(eur_usd, eur_usd_ask, eur_usd_bid,
+                             to_curr(1000, precision), 10, precision,
+                             ignore_spread=False)
+    simulation1.run()
+    simulation2.run()
     # plt.plot(simulation.ma_record)
     plt.plot(from_curr(simulation1.balance_record, precision))
+    plt.plot(from_curr(simulation2.balance_record, precision))
     plt.show()
